d88d874f6239b5f81df5932656cec21f256855e1
ndr_brt <Andrea.Bertagnolli@partner.bmwgroup.com>
2021-10-18 12:30:49 +0200

    spi: Replace MetadataStore with AssetIndex

--- samples/04-file-transfer/consumer/build.gradle.kts
+++ samples/04-file-transfer/consumer/build.gradle.kts
@@ -26,8 +26,7 @@ dependencies {
 
     implementation(project(":extensions:in-memory:transfer-store-memory"))
     implementation(project(":extensions:in-memory:policy-registry-memory"))
-    implementation(project(":extensions:in-memory:metadata-memory"))
-    
+
     implementation(project(":extensions:filesystem:configuration-fs"))
     implementation(project(":extensions:iam:iam-mock"))
 
